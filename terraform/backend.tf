99f2cb78128d7c91ee663d0a4c379b9fe70bb426
genusor <genusor@gmail.com>
2021-08-08 06:42:03 +0300

    fix

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
     organization = "genusor_industry"
 
     workspaces {
-      name = "devops-for-programmers-project-lvl3-777"
+      name = "devops-for-programmers-project-lvl3"
     }
   }
 }
